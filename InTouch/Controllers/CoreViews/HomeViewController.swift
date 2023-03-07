2abdbcaa177e6bf4fc0aa9d042aa2a36c89c01f0
jesseandringa <jean4234@colorado.edu>
2023-03-07 15:44:49 -0700

    Initial Commit

---
+++ InTouch/Controllers/CoreViews/HomeViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  InTouch
+//
+//  Created by jesse andringa on 3/7/23.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
